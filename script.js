b271d3e773ee27b497f59cf11af8b73fe21dd8df
sounakroy95 <rsounak.roy@gmail.com>
2022-02-11 21:58:48 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden'); //no dot in this case
+    overlay.classList.remove('hidden');
+  });
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
